4c93fef5ac208546bb3de316e675690ab4a25f89
Akash323yadav <115734510+Akash323yadav@users.noreply.github.com>
2022-10-14 00:29:22 +0530

    find length of array

---
+++ Arrays/arrays2.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+public class arrays2 {
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+        Scanner ac = new Scanner(System.in);
+        System.out.println("length of the array="+marks.length);
+    }
+}
