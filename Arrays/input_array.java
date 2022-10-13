b68041c7b79810edac1f2211e5e8618fd504c9f9
Akash323yadav <115734510+Akash323yadav@users.noreply.github.com>
2022-10-13 23:35:09 +0530

    Create  input_array.java

---
+++ Arrays/input_array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class array_input
+{
+    public static void main(String[]arr)
+    {
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[10];
+        for(int i=0;i<10;i++)
+            arr[i]=sc.nextInt();
+    }
+}
+          
